56e110d80a18eba8c9cca4a401a21a8be98c04a9
Nathaniel Richards <nathrich23@csu.fullerton.edu>
2019-10-14 23:39:35 -0700

    Added php files for a test

---
+++ db_connection.php
@@ -0,0 +1,15 @@
+<?php
+function OpenCon(){
+  $dbhost = "localhost";
+  $dbuser = "root";
+  $dbpass = "TheBoys1234!";
+  $db = "blacklistwebsite";
+
+  $conn = new mysqli ($dbhost,$dbuser, $dbpass, $db) or die("Connection Failed: %s\n". $conn -> error);
+  return $conn;
+}
+
+function CloseCon($conn){
+  $conn -> close();
+}
+ ?>
