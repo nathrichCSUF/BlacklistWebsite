7a3b6cad605172eb56024a39cb0dcade8ca01528
David Lee <626davidlee@gmail.com>
2019-10-15 17:39:35 -0700

    login module

--- js/login.js
+++ js/login.js
@@ -3,11 +3,16 @@
 	'use strict';
 	var App = window.App ||  {};
 	var Validation = {
-		accountValid: function (email, password) {
+		accountValid: function (email, password, db) {
 
 		}
 	}
 	
+
+
+	App.Validation = Validation;
+
+	window.App = App;
 
 
 })(window);
